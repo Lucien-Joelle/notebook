70ede071ae0962e1484e168d26bae48aa2b11fe0
Lucien <3454410481@qq.com>
2025-07-03 22:06:17 +0800

    修复数学公式问题

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,31 +1,16 @@
 window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"], ["$", "$"]],
-      displayMath: [["\\[", "\\]"], ["$$", "$$"]],
-      processEscapes: true,
-      processEnvironments: true,
-      packages: {'[+]': ['base', 'ams', 'noerrors', 'noundefined', 'cancel', 'enclose', 'verb', 'tags', 'newcommand', 'unicode']},
-      macros: {
-        sideset: ["\\mathop{\\mathop{#3}\\limits^{#1}_{#2}}", 3]
-      }
-    },
-    options: {
-      ignoreHtmlClass: "tex2jax_ignore",
-      processHtmlClass: ".*"
-    },
-    loader: {
-      load: ['[tex]/ams', '[tex]/cancel', '[tex]/enclose', '[tex]/verb', '[tex]/tags', '[tex]/newcommand', '[tex]/unicode']
-    },
-    startup: {
-      ready: () => {
-        MathJax.startup.defaultReady();
-        MathJax.startup.promise.then(() => {
-          console.log('MathJax initial typesetting complete');
-        });
-      }
-    }
-  };
-  
-  document$.subscribe(() => { 
-    MathJax.typesetPromise()
-  })
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+  MathJax.typesetPromise()
+})
